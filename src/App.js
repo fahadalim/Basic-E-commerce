49cbcaf310886785c6ffc2b22821cb43e801ffe6
fahadalim <mohammadfahadalim@gmail.com>
2022-07-18 01:00:48 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Banner } from "./components/Banner";
+import { Products } from "./components/Products";
+import { ProductsContextProvider } from "./global/ProductContext";
+import { CartContextProvider } from "./global/CartContext";
+import { Cart } from "./components/cart";
+import {NotFound} from "./components/NotFound"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductsContextProvider>
+        <CartContextProvider>
+        <Navbar></Navbar>
+        <Banner></Banner>
+        <Routes>
+          <Route path="/" element={<Products></Products>} />
+          <Route path="/cart" element={<Cart></Cart>}></Route>
+          <Route path="*" element={<NotFound></NotFound>} />
+        </Routes>
+        </CartContextProvider>
+      </ProductsContextProvider>
     </div>
   );
 }
